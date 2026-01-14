05dad04e1c5113e6d25aeca2a69ec82a569c838c
ryan-XY <ryan.zheng@echox.io>
2026-01-14 16:22:15 +0800

    feat: zh_TW translations
    (cherry picked from commit 2751390c514b375245631180134bcd26985efde2)

--- packages/panels/resources/lang/zh_TW/auth/pages/login.php
+++ packages/panels/resources/lang/zh_TW/auth/pages/login.php
@@ -6,6 +6,19 @@ return [
 
     'heading' => '登入帳號',
 
+    'actions' => [
+
+        'register' => [
+            'before' => '或',
+            'label' => '註冊帳號',
+        ],
+
+        'request_password_reset' => [
+            'label' => '忘記密碼？',
+        ],
+
+    ],
+
     'form' => [
 
         'email' => [
@@ -30,6 +43,30 @@ return [
 
     ],
 
+    'multi_factor' => [
+
+        'heading' => '驗證您的身份',
+
+        'subheading' => '為了繼續登入，您需要驗證您的身份。',
+
+        'form' => [
+
+            'provider' => [
+                'label' => '您想要如何驗證？',
+            ],
+
+            'actions' => [
+
+                'authenticate' => [
+                    'label' => '確認登入',
+                ],
+
+            ],
+
+        ],
+
+    ],
+
     'messages' => [
 
         'failed' => '所提供的帳號密碼與資料庫中的記錄不相符。',
@@ -39,7 +76,8 @@ return [
     'notifications' => [
 
         'throttled' => [
-            'title' => '嘗試登入次數過多。請在 :seconds 秒後重試。',
+            'title' => '嘗試登入次數過多',
+            'body' => '請在 :seconds 秒後重試。',
         ],
 
     ],
